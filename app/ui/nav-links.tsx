755eea32acef460fb40a1cb7d6d243c5e16bf48f
sunseehray <sabtirazona@byui.edu>
2024-03-08 13:33:55 +0800

    added fonts patrick hand and lato

--- app/ui/nav-links.tsx
+++ app/ui/nav-links.tsx
@@ -5,6 +5,7 @@ import {
     UserCircleIcon
   } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import { lato } from '@/app/ui/fonts';
   
   // Map of links to display in the side navigation.
   // Depending on the size of the application, this would be stored in a database.
@@ -40,7 +41,7 @@ import Link from 'next/link';
             <Link
               key={link.name}
               href={link.href}
-              className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-yellow-300 md:flex-none md:justify-start md:p-2 md:px-3"
+              className={`${lato.className} flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-amber-300 md:flex-none md:justify-start md:p-2 md:px-3`}
             >
               <LinkIcon className="w-6" />
               <p className="hidden md:block">{link.name}</p>
