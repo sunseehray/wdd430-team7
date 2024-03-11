3080d3d6f960fb9f2223396d5ce02d8b97bcf8b5
sunseehray <sabtirazona@byui.edu>
2024-03-11 11:42:05 +0800

    general/root layout started

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import '@/app/ui/global.css';
-import { inter, habibi, patrick_hand, lato } from '@/app/ui/fonts';
+import { patrick_hand, lato } from '@/app/ui/fonts';
 import SideNav from '@/app/ui/dashboard/sidenav';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -13,7 +13,36 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${lato.className} antialiased`}>
-        {children}
+        <main className="flex min-h-screen flex-row">
+          <div className="flex flex-col bg-orange-500 p-4 gap-4 md:w-1/6">
+            <Link 
+              href={"/"}
+              className="flex h-30 items-center justify-center rounded-lg p-4 md:h-30 priority"
+            >  
+              <Image 
+                src="/images/handicraft_haven_logo.png"
+                width={75}
+                height={75}
+                priority={true}
+                className="rounded-full"
+                alt="Handcraft Haven Logo"
+              />
+            </Link>
+            <NavLinks/>
+          </div>
+          <div className="flex grow flex-col">
+            <div className="flex flex-row items-begin justify-between rounded-1g p-4 gap-4 priority">
+              <h1 className={`${patrick_hand.className} text-orange-500 text-4xl text-left`}>Handcrafted Haven</h1>
+              <p>Search bar</p>
+              <p>Notifications Icon</p>
+              <p>Cart icon</p>
+              <p>User icon</p>
+            </div>
+            <div className="flex items-center justify-center bg-blue-300 p-4 md:block md:grow">
+              {children}
+            </div>
+          </div>
+        </main>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,14 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
-import { inter, habibi } from '@/app/ui/fonts';
+import { inter, habibi, patrick_hand } from '@/app/ui/fonts';
 import Image from 'next/image';
+import NavLinks from './ui/nav-links';
 
 export default function Page() {
   return (
-    <p>Home</p>
+        <div>
+          Widgets
+        </div>
   );
 }
